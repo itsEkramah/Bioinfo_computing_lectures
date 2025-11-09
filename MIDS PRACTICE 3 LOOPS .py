8759a185a17f9944068c5d8dc5fcf7226283a9da
Muhammad Ekramah <muhammadekramah786@gmail.com>
2025-11-09 14:52:55 +0500

    .

---
+++ MIDS PRACTICE 3 LOOPS .py
@@ -0,0 +1,63 @@
+"""Topic 3: Loops in Python
+🎯 Learning Goals
+
+By the end of this topic, you should confidently handle:
+
+for and while loops
+
+range() function
+
+break, continue, and pass
+
+Nested loops
+
+The while...else and for...else blocks
+
+Common loop-based logic (factorial, sum, reverse number, pattern printing, etc.)"""
+
+
+# for i in range(3):
+#     print( "Python" ,  end="  " )
+#     ###end" "  ka use krnay se same line main print hota warna  hrr row main 1 baar 
+
+
+# for i in range(2, 10, 3):
+#     print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+# ###print  apple banana cherry in seperate rows 
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     # i =+ 1   infinite loop of printing 1
+#     i +=1     ##   print 1-5 in rows 
+
+
+# break
+
+# # Stops loop immediately.
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)
+# ## 0 1 2 print ho gaa seperate rows main hio
+# q k loop jese hi 3 k equalhoa brak ho gea aur 3 ki value i main aai hi nahii
+
+
+# continue
+
+# Skips current iteration and moves to next.
+# for i in range(5):
+#     if i == 2:
+#         continue
+#     print(i)
+
+
+# pass
+
+# Does nothing (placeholder for future logic).
+for i in range(3):
+    pass
